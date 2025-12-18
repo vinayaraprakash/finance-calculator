e4a4f0176afef135c16793b89148c502e83048b5
vinayaraprakash <vinayaraprakash@gmail.com>
2025-12-18 16:00:56 +0530

    putting adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5649172375396609"
+     crossorigin="anonymous"></script>
+      </head>
       <body>
         {/* NAVBAR */}
         <Navbar />
